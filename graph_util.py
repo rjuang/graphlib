61c02b06547237367cc353b9f3061a55392179bf
Ray Juang <radford@radford-macbookpro2.roam.corp.google.com>
2015-02-15 23:25:36 +0800

    Adding utility functions for fetching children/parent nodes, constructing a neighbor mapping, and checking whether two nodes are connected.

--- graph_util.py
+++ graph_util.py
@@ -1,5 +1,5 @@
 def adjacency_matrix(graph, order=None):
-  """ Construct an adjacency matrix from a given graph.
+  """Construct an adjacency matrix from a given graph.
 
   :param graph: a graph_types.GraphType to construct adjacency matrix from.
   :param order: array of node ids containing order of adjacency matrix. If None,
@@ -23,3 +23,121 @@ def adjacency_matrix(graph, order=None):
     result[i][j] = 1
 
   return result
+
+
+def parents(graph, id):
+  """Returns the parents of the node in the graph.
+
+  :param graph: a graph_types.GraphType the node is in.
+  :param id: id value of the node to get parents of.
+  :return: an array of ids corresponding to the parents of the node.
+  """
+  result = set()
+  for e in graph.edges():
+    nodes = e.nodes()
+    if nodes[-1].id() == id:
+      result.add(nodes[0].id())
+  return list(result)
+
+
+def children(graph, id):
+  """Returns the children of the node in the graph.
+
+  :param graph: a graph_types.GraphType the node is in.
+  :param id: id of the node to get children of.
+  :return: an array of ids corresponding to the children of the node.
+  """
+  result = set()
+  for e in graph.edges():
+    nodes = e.nodes()
+    if nodes[0].id() == id:
+      result.add(nodes[-1].id())
+  return list(result)
+
+
+def markov_blanket(graph, id):
+  """Returns the Markov blanket of a node.
+
+   The Markov blanket of a node is defined as the node's parents, children, and
+   parents of children. The node itself is excluded.
+
+  :param graph: a graph_types.GraphType to fetch the markov blanket from.
+  :param id: id of node to get Markov blanket of.
+  :return: an array of nodes representing the Markov blanket of the specified
+  node in the graph.
+  """
+  child_nodes = children(graph, id)
+
+  # Add children.
+  results = set(child_nodes)
+
+  # Add parents of children.
+  for c in child_nodes:
+    if c == id:
+      continue
+    results = results.union(parents(graph, c))
+
+  # Add parents.
+  results = results.union(parents(graph, id))
+
+  # Exclude self.
+  results.discard(id)
+
+  return list(results)
+
+
+def neighbor_map(graph, directed=True):
+  """Construct and return dictionary mapping nodes to a list of its neighbors.
+
+  :param graph: a graph_types.GraphType to fetch neighbors from.
+  :param directed: optional argument (default True) that specifies whether edges
+  should be treated as directed or bi-directional.
+
+  :return: a dictionary mapping a node id to a list of neighboring node ids.
+  """
+  result = {}
+  for e in graph.edges():
+    nodes = e.nodes()
+
+    from_id = nodes[0].id()
+    to_id = nodes[-1].id()
+
+    edges = [(from_id, to_id)]
+    if not directed and from_id != to_id:
+      edges.append((to_id, from_id))
+
+    for id, neighbor_id in edges:
+      if id not in result:
+        result[id] = []
+      result[id].append(neighbor_id)
+  return result
+
+
+def is_connected(graph, start_id, end_id, directed=True):
+  """Returns True if a path exists between the start and end node ids.
+
+  :param graph: a graph_types.GraphType containing information about the graph.
+  :param start_id: starting node id.
+  :param end_id: ending node id.
+  :param directed: optional argument (default True) that specifies whether edges
+  should be treated as directed or bi-directional.
+
+  :return: True if a path exists between start_id and end_id in the graph.
+  """
+  neighbors = neighbor_map(graph, directed=directed)
+  id_queue = [start_id]
+  visited = {}
+  while id_queue:
+    id = id_queue.pop()
+    if id in visited:
+      continue
+    visited[id] = True
+
+    if id not in neighbors:
+      continue
+
+    if end_id in neighbors[id]:
+      return True
+    id_queue.extend(neighbors[id])
+
+  return False
--- graph_util_test.py
+++ graph_util_test.py
@@ -5,6 +5,20 @@ import unittest
 
 class GraphUtilTest(unittest.TestCase):
 
+  def assert_maps_equal(self, expected, actual):
+    self.assertEqual(len(expected), len(actual), msg='Map length mismatch.')
+    for exp_key in expected:
+      self.assertTrue(exp_key in actual, msg='Did not find %s in %s' % (
+        exp_key, actual))
+
+      exp_value = sorted(expected[exp_key])
+      actual_value = sorted(actual[exp_key])
+
+      self.assertEqual(
+        exp_value, actual_value ,
+        msg='Expected element with key %s to contain %s. Contains %s.' % (
+          exp_key, exp_value, actual_value))
+
   def test_adjacency_matrix(self):
     factory = basic_graph.Factory()
     g = graph.from_string(factory, 'A->A, B->B, C->C, E->E, A->E')
@@ -28,6 +42,68 @@ class GraphUtilTest(unittest.TestCase):
       [0, 0, 0, 0, 0]
     ]
     self.assertEqual(expected_adj, adj)
+
+  def test_parents(self):
+    factory = basic_graph.Factory()
+    g = graph.from_string(factory, 'A->A, B->B, C->C, E->E, A->E, F')
+    self.assertEqual(['A'], graph_util.parents(g, 'A'))
+    self.assertEqual(['A', 'E'], sorted(graph_util.parents(g, 'E')))
+    self.assertEqual([], graph_util.parents(g, 'F'))
+
+  def test_children(self):
+    factory = basic_graph.Factory()
+    g = graph.from_string(factory, 'A->A, B->B, C->C, E->E, A->E, F')
+    self.assertEqual(['A', 'E'], sorted(graph_util.children(g, 'A')))
+    self.assertEqual(['E'], graph_util.children(g, 'E'))
+    self.assertEqual([], graph_util.parents(g, 'F'))
+
+  def test_markov_blanket(self):
+    factory = basic_graph.Factory()
+    g = graph.from_string(factory, 'A->A, B->B, C->C, E->E, A->E, F->E')
+    self.assertEqual(['E', 'F'], sorted(graph_util.markov_blanket(g, 'A')))
+
+    g = graph.from_string(
+      factory, 'x1->x8, x1->x4, x2->x4, x3->x4, x4->x5, x4->x6, x7->x6')
+    self.assertEqual(['x1', 'x2', 'x3', 'x5', 'x6', 'x7'],
+                     sorted(graph_util.markov_blanket(g, 'x4')))
+
+  def test_neighbor_map(self):
+    factory = basic_graph.Factory()
+    g = graph.from_string(factory, 'A->A, B->B, C->C, E->E, A->E, F->E')
+    expected_map = {
+      'A': ['A', 'E'],
+      'B': ['B'],
+      'C': ['C'],
+      'E': ['E'],
+      'F': ['E'],
+    }
+    self.assert_maps_equal(expected_map, graph_util.neighbor_map(g))
+
+    expected_map = {
+      'A': ['A', 'E'],
+      'B': ['B'],
+      'C': ['C'],
+      'E': ['A', 'E', 'F'],
+      'F': ['E'],
+    }
+    self.assert_maps_equal(expected_map, graph_util.neighbor_map(
+      g, directed=False))
+
+  def test_is_connected(self):
+    factory = basic_graph.Factory()
+    g = graph.from_string(factory, 'A->A, B->E, C->C, E->E, E->C, F->E')
+    self.assertTrue(graph_util.is_connected(g, 'A', 'A'))
+    self.assertTrue(graph_util.is_connected(g, 'B', 'E'))
+    self.assertTrue(graph_util.is_connected(g, 'E', 'C'))
+    self.assertTrue(graph_util.is_connected(g, 'F', 'E'))
+    self.assertTrue(graph_util.is_connected(g, 'C', 'C'))
+    self.assertTrue(graph_util.is_connected(g, 'B', 'C'))
+    self.assertTrue(graph_util.is_connected(g, 'F', 'C'))
+    self.assertFalse(graph_util.is_connected(g, 'B', 'B'))
+    self.assertFalse(graph_util.is_connected(g, 'F', 'F'))
+    self.assertFalse(graph_util.is_connected(g, 'C', 'B'))
+    self.assertTrue(graph_util.is_connected(g, 'C', 'B', directed=False))
+
 
 if __name__ == '__main__':
   unittest.main()
